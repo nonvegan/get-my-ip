ccfb02c81b99f0304ab5b011706739bdca929e62
pedro <pedro@DESKTOP-9UAVANT>
2019-12-18 02:26:36 +0000

    removed useless leftover code

--- src/FrontEnd/mainWindow.java
+++ src/FrontEnd/mainWindow.java
@@ -17,7 +17,7 @@ public class mainWindow extends javax.swing.JFrame {
     /**
      * Creates new form mainWindow
      */
-    public mainWindow(){
+    public mainWindow() throws ClassNotFoundException {
         initComponents();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
